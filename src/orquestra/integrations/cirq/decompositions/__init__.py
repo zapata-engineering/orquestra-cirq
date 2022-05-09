dce42e3999fb585e9a4285d867a18a6d5afc3805
AthenaCaesura <athena.caesura@zapatacomputing.com>
2022-05-09 18:19:48 -0400

    fix: added whitespace to copyrights

--- src/orquestra/integrations/cirq/decompositions/__init__.py
+++ src/orquestra/integrations/cirq/decompositions/__init__.py
@@ -1,4 +1,4 @@
 ################################################################################
-#© Copyright 2021-2022 Zapata Computing Inc.
+# © Copyright 2021-2022 Zapata Computing Inc.
 ################################################################################
 from ._cirq_decompositions import PowerGateToPhaseAndRotation, decompose_cirq_circuit
