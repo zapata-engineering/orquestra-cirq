8fa9ff5d5067c1589b6ad1b4b1bd1922eb864506
AthenaCaesura <athena.caesura@zapatacomputing.com>
2022-05-09 14:21:29 -0400

    Add Copyright for ticket: zqs-1011-1

--- src/orquestra/integrations/cirq/conversions/__init__.py
+++ src/orquestra/integrations/cirq/conversions/__init__.py
@@ -1,5 +1,5 @@
 ################################################################################
-# © Copyright 2021 Zapata Computing Inc.
+#© Copyright 2021-2022 Zapata Computing Inc.
 ################################################################################
 from ._circuit_conversions import export_to_cirq, import_from_cirq
 from ._openfermion_conversions import qubitop_to_paulisum
