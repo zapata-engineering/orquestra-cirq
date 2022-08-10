6b0d3a09d4858dac3e22cb131688d4d059a0d7b9
unknown <laura.gao@zapatacomputing.com>
2022-08-10 09:35:12 -0400

    Improve tests for conversions

--- tests/orquestra/integrations/cirq/conversions/openfermion_conversions_test.py
+++ tests/orquestra/integrations/cirq/conversions/openfermion_conversions_test.py
@@ -11,47 +11,45 @@ class TestOpenFermionConversions:
         "pauli_op",
         [PauliTerm.identity() * (4 + 0.5j), PauliSum.identity() * (4 + 0.5j)],
     )
-    def test_to_openfermion_identity_operator(self, pauli_op):
-        assert to_openfermion(pauli_op, QubitOperator) == QubitOperator("", 4 + 0.5j)
-        assert to_openfermion(pauli_op, IsingOperator) == IsingOperator("", 4 + 0.5j)
+    def test_identity_operator(self, pauli_op):
+        openfermion_op = to_openfermion(pauli_op, QubitOperator)
+        openfermion_ising_op = to_openfermion(pauli_op, IsingOperator)
+        assert openfermion_op == QubitOperator("", 4 + 0.5j)
+        assert openfermion_ising_op == IsingOperator("", 4 + 0.5j)
 
-    @pytest.mark.parametrize(
-        "openfermion_op", [QubitOperator("", 4 + 0.5j), IsingOperator("", 4 + 0.5j)]
-    )
-    def test_from_openfermion_identity_operator(self, openfermion_op):
-        pauli_op = from_openfermion(openfermion_op)
-        assert pauli_op == PauliTerm.identity() * (4 + 0.5j)
+        back_to_pauli_op = from_openfermion(openfermion_op)
+        back_to_pauli_op_from_ising = from_openfermion(openfermion_ising_op)
+        assert back_to_pauli_op == pauli_op
+        assert back_to_pauli_op_from_ising == pauli_op
 
     @pytest.mark.parametrize("pauli_op", [PauliTerm.identity() * 0, PauliSum()])
-    def test_to_openfermion_zero(self, pauli_op):
-        assert to_openfermion(pauli_op, QubitOperator) == QubitOperator()
-        assert to_openfermion(pauli_op, IsingOperator) == IsingOperator()
+    def test_zero_operator(self, pauli_op):
+        of_op = to_openfermion(pauli_op, QubitOperator)
+        of_op_ising = to_openfermion(pauli_op, IsingOperator)
+        assert of_op == QubitOperator()
+        assert of_op_ising == IsingOperator()
 
-    @pytest.mark.parametrize("openfermion_op", [QubitOperator(), IsingOperator()])
-    def test_from_openfermion_zero(self, openfermion_op):
-        pauli_op = from_openfermion(openfermion_op)
-        assert pauli_op == PauliSum()
+        back_to_pauli_op = from_openfermion(of_op)
+        back_to_pauli_op_from_ising = from_openfermion(of_op_ising)
+        assert back_to_pauli_op == pauli_op
+        assert back_to_pauli_op_from_ising == pauli_op
 
     @pytest.mark.parametrize(
         "pauli_op", [PauliTerm("X0*Y2*Z3", 9 + 0.5j), PauliSum("(9+0.5j)*X0*Y2*Z3")]
     )
-    def test_to_openfermion_single_term(self, pauli_op):
+    def test_single_term(self, pauli_op):
         openfermion_op = to_openfermion(pauli_op)
         assert openfermion_op == QubitOperator("X0 Y2 Z3", 9 + 0.5j)
+        back_to_pauli_op = from_openfermion(openfermion_op)
+        assert back_to_pauli_op == pauli_op
 
     @pytest.mark.parametrize(
-        "pauli_op", [PauliTerm("Z0*Z2*Z3", 1 + 3.5j), PauliSum("(1+3.5j)*Z0*Z2*Z3")]
+        "pauli_op", [PauliTerm("Z0*Z2*Z3", 2j), PauliSum("(2j)*Z0*Z2*Z3")]
     )
-    def test_to_openfermion_ising_single_term(self, pauli_op):
-        openfermion_op = to_openfermion(pauli_op, IsingOperator)
-        assert openfermion_op == IsingOperator("Z0 Z2 Z3", 1 + 3.5j)
-
-    def test_from_openfermion_single_term(self):
-        openfermion_op = QubitOperator("X0 Y2 Z3", 9 + 0.5j)
-        pauli_op = from_openfermion(openfermion_op)
-        assert pauli_op == PauliTerm("X0*Y2*Z3", 9 + 0.5j)
-        ising_op = IsingOperator("Z0 Z2 Z3", 2j)
-        assert from_openfermion(ising_op) == PauliTerm("(2j)*Z0*Z2*Z3")
+    def test_ising_single_term(self, pauli_op):
+        ising_op = to_openfermion(pauli_op, IsingOperator)
+        assert ising_op == IsingOperator("Z0 Z2 Z3", 2j)
+        assert from_openfermion(ising_op) == pauli_op
 
     def test_multi_term(self):
         pauli_op = PauliSum("2*Z0*X5*Y7 + 8*Z5")
@@ -59,13 +57,14 @@ class TestOpenFermionConversions:
         assert to_openfermion(pauli_op) == openfermion_op
         assert from_openfermion(openfermion_op) == pauli_op
 
+    def test_ising_multi_term(self):
         ising_op = PauliSum("2*Z0*Z5*Z7 + 8*Z5")
         openfermion_ising_op = IsingOperator("2 [Z0 Z5 Z7] + 8 [Z5]")
         assert to_openfermion(ising_op, IsingOperator) == openfermion_ising_op
         assert from_openfermion(openfermion_ising_op) == ising_op
 
     def test_random_qubit_op(self):
-        qubit_op = random_qubit_operator()
-        transformed_op = from_openfermion(qubit_op)
-        retransformed_op = to_openfermion(transformed_op)
-        assert qubit_op == retransformed_op
+        of_qubit_op = random_qubit_operator()
+        pauli_op = from_openfermion(of_qubit_op)
+        back_to_of_qubit_op = to_openfermion(pauli_op)
+        assert of_qubit_op == back_to_of_qubit_op
