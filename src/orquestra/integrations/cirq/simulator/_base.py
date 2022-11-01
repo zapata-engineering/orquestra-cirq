f4cd606724599b79d99ef175f44cb662e9b29ce9
boniface316 <boniface.yogi@gmail.com>
2022-11-01 12:47:44 -0400

    refactor: refactored the code to fit the new API

--- src/orquestra/integrations/cirq/simulator/_base.py
+++ src/orquestra/integrations/cirq/simulator/_base.py
@@ -3,48 +3,20 @@
 ################################################################################
 
 import sys
-from typing import List, Sequence, cast
+from typing import List, Optional, Sequence, cast
 
 import cirq
 import numpy as np
-from orquestra.quantum.api.backend import QuantumSimulator, StateVector
+from orquestra.quantum.api.wavefunction_simulator import BaseWavefunctionSimulator
 from orquestra.quantum.circuits import Circuit, I
-from orquestra.quantum.measurements import (
-    ExpectationValues,
-    Measurements,
-    expectation_values_to_real,
-)
+from orquestra.quantum.measurements import ExpectationValues, expectation_values_to_real
 from orquestra.quantum.operators import PauliRepresentation, get_sparse_operator
+from orquestra.quantum.typing import StateVector
 
 from ..conversions import export_to_cirq
 
 
-def _prepare_measurable_cirq_circuit(circuit, noise_model):
-    """
-    Export circuit to Cirq and add terminal measurements.
-
-    Args:
-        circuit (orquestra.quantum.circuit.Circuit): the circuit to prepare the state.
-        noise_model: model to create a noisy circuit
-
-    Returns:
-        circuit to run on a cirq or qsim simulator
-    """
-
-    for i in range(circuit.n_qubits):
-        circuit += I(i)
-
-    cirq_circuit = export_to_cirq(circuit)
-
-    if noise_model is not None:
-        cirq_circuit = cirq_circuit.with_noise(noise_model)
-
-    cirq_circuit.append(cirq.measure_each(*cirq_circuit.all_qubits()))
-
-    return cirq_circuit
-
-
-class CirqBasedSimulator(QuantumSimulator):
+class CirqBasedSimulator(BaseWavefunctionSimulator):
 
     supports_batching = True
     batch_size = sys.maxsize
@@ -70,102 +42,6 @@ class CirqBasedSimulator(QuantumSimulator):
         self.simulator = simulator
         self.param_resolver = param_resolver
         self.qubit_order = qubit_order
-
-    def run_circuit_and_measure(self, circuit: Circuit, n_samples: int) -> Measurements:
-        """Run a circuit and measure a certain number of bitstrings.
-
-        Args:
-            circuit: the circuit to prepare the state.
-            n_samples: number of bitstrings to measure. If None, `self.n_samples`
-                is used.
-        Returns:
-            A list of bitstrings.
-        """
-        super().run_circuit_and_measure(circuit, n_samples)
-
-        result_object = self.simulator.run(
-            _prepare_measurable_cirq_circuit(circuit, self.noise_model),
-            param_resolver=self.param_resolver,
-            repetitions=n_samples,
-        )
-
-        measurement = get_measurement_from_cirq_result_object(
-            result_object, circuit.n_qubits, n_samples
-        )
-
-        return measurement
-
-    def run_circuitset_and_measure(
-        self, circuitset: Sequence[Circuit], n_samples: Sequence[int]
-    ) -> List[Measurements]:
-        """Run a set of circuits and measure a certain number of bitstrings.
-        Args:
-            circuitset: a set of circuits to prepare the state.
-            n_samples: number of bitstrings to measure. If None, `self.n_samples`
-                is used. If an iterable, its-ith element corresponds to number
-                of samples that will be taken from i-th circuit. If an int N,
-                each circuit in circuitset will be measured N times.
-        Returns:
-            a list of lists of bitstrings (a list of lists of tuples)
-        """
-        super().run_circuitset_and_measure(circuitset, n_samples)
-
-        cirq_circuitset = [
-            _prepare_measurable_cirq_circuit(circuit, self.noise_model)
-            for circuit in circuitset
-        ]
-
-        simulation_result = self.simulator.run_batch(
-            cirq_circuitset, repetitions=list(n_samples)
-        )
-
-        measurements_set = [
-            get_measurement_from_cirq_result_object(
-                sub_result[0], circuit.n_qubits, num_samples
-            )
-            for sub_result, circuit, num_samples in zip(
-                simulation_result, circuitset, n_samples
-            )
-        ]
-
-        return measurements_set
-
-    def get_exact_expectation_values(
-        self, circuit: Circuit, qubit_operator: PauliRepresentation
-    ) -> ExpectationValues:
-        """Compute exact expectation values with respect to given operator.
-
-        Args:
-            circuit: the circuit to prepare the state
-            qubit_operator: the operator to measure
-        Returns:
-            the expectation values of each term in the operator
-        """
-        if self.noise_model is not None:
-            return self.get_exact_noisy_expectation_values(circuit, qubit_operator)
-        else:
-            wavefunction = self.get_wavefunction(circuit).amplitudes
-
-            # Pyquil does not support PauliSums with no terms.
-            if len(qubit_operator.terms) == 0:
-                return ExpectationValues(np.zeros((0,)))
-
-            values = []
-
-            for pauli_term in qubit_operator.terms:
-                sparse_pauli_term_ndarray = get_sparse_operator(
-                    pauli_term, n_qubits=circuit.n_qubits
-                ).toarray()
-                if np.size(sparse_pauli_term_ndarray) == 1:
-                    expectation_value = sparse_pauli_term_ndarray[0][0]
-                    values.append(expectation_value)
-                else:
-                    expectation_value = np.real(
-                        wavefunction.conj().T @ sparse_pauli_term_ndarray @ wavefunction
-                    )
-                    values.append(expectation_value)
-
-            return expectation_values_to_real(ExpectationValues(np.asarray(values)))
 
     def get_exact_noisy_expectation_values(
         self, circuit: Circuit, qubit_operator: PauliRepresentation
@@ -212,6 +88,10 @@ class CirqBasedSimulator(QuantumSimulator):
     def _get_wavefunction_from_native_circuit(
         self, circuit: Circuit, initial_state: StateVector
     ) -> StateVector:
+        # Cirq does not allow inactive qubits so we added identity to our circuits.
+        for i in range(circuit.n_qubits):
+            circuit += I(i)
+
         cirq_circuit = cast(cirq.Circuit, export_to_cirq(circuit))
 
         initial_state = np.array(initial_state, np.complex64)
@@ -227,33 +107,3 @@ class CirqBasedSimulator(QuantumSimulator):
 
     def _extract_density_matrix(self, result):
         return result.density_matrix_of()
-
-
-def get_measurement_from_cirq_result_object(
-    result_object: cirq.Result, n_qubits: int, n_samples: int
-) -> Measurements:
-    """Extract measurement bitstrings from cirq result object.
-
-    Args:
-        result_object: object returned by Cirq simulator's run or run_batch.
-        n_qubits: number of qubits in full circuit (before exporting to cirq).
-        n_samples: number of measured samples
-    Return:
-        Measurements.
-    """
-    keys = _find_reverse_permutation(result_object.data.columns, n_qubits)
-
-    samples = list(
-        tuple(measurement) for measurement in result_object.data.to_numpy()[:, keys]
-    )
-
-    measurement = Measurements(samples)
-    return measurement
-
-
-def _find_reverse_permutation(permutation, n_qubits):
-    keys_to_indices = {f"q({n})": n for n in range(n_qubits)}
-    result = [0 for _ in range(n_qubits)]
-    for i, key in enumerate(permutation):
-        result[keys_to_indices[key]] = i
-    return result
--- tests/orquestra/integrations/cirq/simulator/qsimsimulator_test.py
+++ tests/orquestra/integrations/cirq/simulator/qsimsimulator_test.py
@@ -6,12 +6,6 @@ import sys
 import numpy as np
 import pytest
 from cirq import depolarize
-
-# from cirq import depolarize
-from orquestra.quantum.api.backend_test import (
-    QuantumSimulatorGatesTest,
-    QuantumSimulatorTests,
-)
 from orquestra.quantum.circuits import CNOT, Circuit, H, X
 from orquestra.quantum.operators import PauliSum
 
@@ -29,21 +23,22 @@ def wf_simulator():
 
 
 @pytest.mark.skipif(
-    sys.version_info > (3, 9),
+    sys.version_info > (3, 10),
     reason="QsimSimulator does not work on Python 3.10 or above",
 )
-class TestQsimSimulator(QuantumSimulatorTests):
+# class TestQsimSimulator(QuantumSimulatorTests):
+class TestQsimSimulator:
     def test_setup_basic_simulators(self):
         simulator = QSimSimulator()
         assert isinstance(simulator, QSimSimulator)
         assert simulator.noise_model is None
 
-    def test_run_circuit_and_measure(self):
+    def test_run_and_measure(self):
         # Given
         circuit = Circuit([X(0), CNOT(1, 2)])
         simulator = QSimSimulator()
 
-        measurements = simulator.run_circuit_and_measure(circuit, n_samples=100)
+        measurements = simulator.run_and_measure(circuit, n_samples=100)
         assert len(measurements.bitstrings) == 100
 
         for measurement in measurements.bitstrings:
@@ -53,20 +48,20 @@ class TestQsimSimulator(QuantumSimulatorTests):
         # Given
         circuit = Circuit([X(0), CNOT(1, 2)], n_qubits=4)
         simulator = QSimSimulator()
-        measurements = simulator.run_circuit_and_measure(circuit, n_samples=100)
+        measurements = simulator.run_and_measure(circuit, n_samples=100)
         assert len(measurements.bitstrings) == 100
 
         for measurement in measurements.bitstrings:
             assert measurement == (1, 0, 0, 0)
 
-    def test_run_circuitset_and_measure(self):
+    def test_run_batch_and_measure(self):
         # Given
         simulator = QSimSimulator()
         circuit = Circuit([X(0), CNOT(1, 2)])
         n_circuits = 5
         n_samples = 100
         # When
-        measurements_set = simulator.run_circuitset_and_measure(
+        measurements_set = simulator.run_batch_and_measure(
             [circuit] * n_circuits, n_samples=[100] * n_circuits
         )
         # Then
@@ -93,21 +88,6 @@ class TestQsimSimulator(QuantumSimulatorTests):
             wavefunction.amplitudes[7], (1 / np.sqrt(2) + 0j), atol=10e-15
         )
 
-    def test_get_exact_expectation_values(self):
-        # Given
-        simulator = QSimSimulator()
-        circuit = Circuit([H(0), CNOT(0, 1), CNOT(1, 2)])
-        qubit_operator = PauliSum("2*I0 + -1*Z0*Z1 + X0*X2")
-        target_values = np.array([2.0, -1.0, 0.0])
-
-        # When
-
-        expectation_values = simulator.get_exact_expectation_values(
-            circuit, qubit_operator
-        )
-        # Then
-        np.testing.assert_array_almost_equal(expectation_values.values, target_values)
-
     def test_get_noisy_exact_expectation_values(self):
         # Given
         noise = 0.0002
@@ -127,15 +107,15 @@ class TestQsimSimulator(QuantumSimulatorTests):
             expectation_values.values[1], target_values[1], decimal=2
         )
 
-    def test_run_circuit_and_measure_seed(self):
+    def test_run_and_measure_seed(self):
         # Given
         circuit = Circuit([X(0), CNOT(1, 2)])
         simulator1 = QSimSimulator(seed=12)
         simulator2 = QSimSimulator(seed=12)
 
         # When
-        measurements1 = simulator1.run_circuit_and_measure(circuit, n_samples=1000)
-        measurements2 = simulator2.run_circuit_and_measure(circuit, n_samples=1000)
+        measurements1 = simulator1.run_and_measure(circuit, n_samples=1000)
+        measurements2 = simulator2.run_and_measure(circuit, n_samples=1000)
 
         # Then
         for (meas1, meas2) in zip(measurements1.bitstrings, measurements2.bitstrings):
@@ -154,13 +134,3 @@ class TestQsimSimulator(QuantumSimulatorTests):
         # Then
         for (ampl1, ampl2) in zip(wavefunction1.amplitudes, wavefunction2.amplitudes):
             assert ampl1 == ampl2
-
-
-@pytest.mark.skipif(
-    sys.version_info > (3, 9),
-    reason="QsimSimulator does not work on Python 3.10 or above",
-)
-class TestQSimSimulatorGates(QuantumSimulatorGatesTest):
-    atol_wavefunction = 5e-7
-
-    pass
