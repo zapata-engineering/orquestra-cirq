23d8fcc55a75c4e8fe2892ef65d2d3c92b61abd1
boniface316 <boniface.yogi@gmail.com>
2022-11-10 12:36:39 -0500

    refactor(tests): single file to run both qsim and cirq test

--- src/orquestra/integrations/cirq/simulator/simulator.py
+++ src/orquestra/integrations/cirq/simulator/simulator.py
@@ -2,6 +2,7 @@
 # © Copyright 2021-2022 Zapata Computing Inc.
 ################################################################################
 import sys
+from typing import Union
 
 import cirq
 
@@ -33,7 +34,7 @@ class CirqSimulator(CirqBasedSimulator):
     def __init__(
         self,
         noise_model: cirq.NOISE_MODEL_LIKE = None,
-        seed: int = None,
+        seed: Union[int, None] = None,
         param_resolver: "cirq.ParamResolverOrSimilarType" = None,
         qubit_order=cirq.ops.QubitOrder.DEFAULT,
     ):
